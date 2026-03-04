d2d73ef9237a1618a3d31cd825897f29a44f60a2
Jacobs David <jacobsdavid.dr@gmail.com>
2026-03-04 13:52:08 +0100

    01: advent-calendar-list

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,54 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import DayItem from "@/components/core/day-item";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { FlatList, StyleSheet, View } from "react-native";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
+
+const days = Array.from({ length: 24 });
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      {/* <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
+      </Stack> */}
+      <View style={styles.container}>
+        <FlatList
+          data={days}
+          contentContainerStyle={styles.content}
+          columnWrapperStyle={styles.column}
+          numColumns={2}
+          renderItem={({ item, index }) => <DayItem day={index} />}
+        />
+      </View>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+  column: {
+    gap: 10,
+  },
+});
